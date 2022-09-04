5a1c0fe99b6db65a103319ceaf5b4c9ee23a5d4f
StepanHovsepyan7 <stpnhovsepyan@gmail.com>
2022-09-04 10:39:04 +0400

    Gallery

--- js/script.js
+++ js/script.js
@@ -28,6 +28,13 @@ $(function(){
     let inst = $('.inst')
     let twitr = $('.twitr')
     let book = $('.book'); 
+    let a = $('a');
+    let homeA = $('.homeA');
+    let port = $('.port');
+    let aboutA = $('.aboutA')
+    let galleryA = $('.galleryA')
+    let serviceA = $('.serviceA')
+    let contactA = $('.contactA')
     // let modalNav = $('.modalNav');
     let  fax = $(' .fa-x');
 
@@ -60,14 +67,164 @@ $(function(){
             modalNavV.css({
                 display: 'none',
             })
-            
         })
     }
 
+
+aboutA.click(function(){
+    aboutA.css({
+        color:'#34e3c9' ,
+    })   
+    homeA.css({
+        color: 'white',
+    })
+    port.css({
+        color: 'white',
+    })
+    galleryA.css({
+        color:'white' , 
+    })
+    serviceA.css({
+        color: 'white',
+    })
+    contactA.css({
+        color: 'white',
+    })
+
+})
+
+homeA.click(function () {
+    homeA.css({
+        color:'#34e3c9',
+    }) 
+    aboutA.css({
+        color:'white' ,
+    })   
+    port.css({
+        color: 'white',
+    })
+    galleryA.css({
+        color:'white' , 
+    })
+    serviceA.css({
+        color: 'white',
+    })
+    contactA.css({
+        color: 'white',
+    })
+})
+
+port.click(function () {
+    homeA.css({
+        color:'white',
+    }) 
+    aboutA.css({
+        color:'white' ,
+    })   
+    port.css({
+        color: '#34e3c9',
+    })
+    galleryA.css({
+        color:'white' , 
+    })
+    serviceA.css({
+        color: 'white',
+    })
+    contactA.css({
+        color: 'white',
+    })
 })
 
 
+galleryA.click(function () {
+    homeA.css({
+        color:'white',
+    }) 
+    aboutA.css({
+        color:'white' ,
+    })   
+    port.css({
+        color: 'white',
+    })
+    galleryA.css({
+        color:'#34e3c9' , 
+    })
+    serviceA.css({
+        color: 'white',
+    })
+    contactA.css({
+        color: 'white',
+    })
+})
 
 
+serviceA.click(function () {
+    homeA.css({
+        color:'white',
+    }) 
+    aboutA.css({
+        color:'white' ,
+    })   
+    port.css({
+        color: 'white',
+    })
+    galleryA.css({
+        color:'wihite' , 
+    })
+    serviceA.css({
+        color: '#34e3c9',
+    })
+    contactA.css({
+        color: 'white',
+    })
+})
 
+contactA.click(function () {
+    homeA.css({
+        color:'white',
+    }) 
+    aboutA.css({
+        color:'white' ,
+    })   
+    port.css({
+        color: 'white',
+    })
+    galleryA.css({
+        color:'wihite' , 
+    })
+    serviceA.css({
+        color: 'white',
+    })
+    contactA.css({
+        color: '#34e3c9',
+    })
+})
+
+})
+
+let  aboutClass = $('.aboutClass')
+   
+
+$(window).scroll(function(){
+    $(".aboutClass").each(function(){ 
+      var x = $(window).scrollTop() + $(window).height(); 
+      var y = $(this).offset().top + $(this).height() / 2; 
+
+      if (x >= y) { 
+        $(this).addClass('active');
+        aboutClass.css({
+            display: 'block',
+        })
+      };
+    });
+  });
+
+  let allBTN = $('.allBTN');
+  let foodsBTN = $('.foodsBTN')
+  let drinksBTN = $('.drinksBTN');
+  let eventsBTN = $('.eventsBTN');
+  let pastiresBTN = $('.pastiresBTN');
+
+
+ 
 // .fa-x
